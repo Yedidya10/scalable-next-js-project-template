484bcb5e5b7909b66870e03be28e56af6be2672a
Yedidya Averjel <yedidya.abergel@gmail.com>
2024-05-02 18:45:09 +0300

    refactor: add comment to MongoDB connection

--- src/models/dbModels.ts
+++ src/models/dbModels.ts
@@ -3,11 +3,13 @@ import { User } from './UserModel'
 
 mongoose.Promise = global.Promise
 
-// Get the MongoDB URI from the environment variables and connect to the database
+// Get the MongoDB URI from the environment variables
 const mongoUri =
   process.env.NODE_ENV === 'production'
     ? process.env.MONGODB_URI
     : process.env.MONGODB_URI_LOCAL || ''
+
+// Create a function to establish the connection to the database
 const connectToDatabase = async () => {
   try {
     await mongoose.connect(mongoUri as string)
